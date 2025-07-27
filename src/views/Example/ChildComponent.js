cdb0d2b946f4a0078ecea60a8616a8101aa9ecde
Hue Nhu <hnhu07012004@gmail.com>
2025-07-27 07:27:22 -0700

    Lesson #14: Conditional Output

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -21,23 +21,49 @@ import React from "react";
 //     }
 // }
 
-const ChildComponent = (props) => {
-    let { arrJobs } = props;
-    console.log(">>> Props: ", props);
+class ChildComponent extends React.Component {
+    state = {
+        showJobs: false,
+    };
 
-    return (
-        <>
-            <div className="job-lists">
-                {arrJobs.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            {item.title} - {item.salary}
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs,
+        });
+    };
+    render() {
+        let { arrJobs } = this.props;
+        let { showJobs } = this.state;
+        let checkShowJobs = showJobs ? "showJobs == true" : "showJobs == false";
+        {
+            console.log(">>> check showJobs: ", checkShowJobs);
+        }
+
+        return (
+            <>
+                {!showJobs ? (
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                ) : (
+                    <>
+                        <div className="job-lists">
+                            {arrJobs.map((item) => {
+                                return (
+                                    <div key={item.id}>
+                                        {item.title} - {item.salary}
+                                    </div>
+                                );
+                            })}
                         </div>
-                    );
-                })}
-            </div>
-        </>
-    );
-};
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>Hide</button>
+                        </div>
+                    </>
+                )}
+            </>
+        );
+    }
+}
 
 export default ChildComponent;
