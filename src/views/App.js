97c8d2f47e32d33f49538fafd96691ae2369e14e
Hue Nhu <hnhu07012004@gmail.com>
2025-07-28 12:32:26 -0700

    Lesson #21.1: TodoApp (create/read)

--- src/views/App.js
+++ src/views/App.js
@@ -1,17 +1,33 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import MyComponent from "./Example/MyComponent";
+// import MyComponent from "./Example/MyComponent";
+import ToDoList from "./Todos/ToDoList";
+import { ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello world! ;)</p>
-                <MyComponent />
-            </header>
-        </div>
-    );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>Tako's To Do List</p>
+				<ToDoList />
+			</header>
+			<ToastContainer
+				position='top-right'
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick={false}
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme='light'
+				transition={Bounce}
+			/>
+		</div>
+	);
 }
 
 export default App;
