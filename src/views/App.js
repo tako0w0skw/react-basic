ab4ac9dd887b16a2cb0d7b57137b7320ca64c6ad
Hue Nhu <hnhu07012004@gmail.com>
2025-07-29 08:44:48 -0700

    Lesson #23: Links & Navlinks (React Router)

--- src/views/App.js
+++ src/views/App.js
@@ -1,36 +1,30 @@
 import logo from "./logo.svg";
 import "./App.scss";
-// import MyComponent from "./Example/MyComponent";
 import ToDoList from "./Todos/ToDoList";
 import { ToastContainer, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Nav from "./Nav/Nav";
 import Home from "./Example/Home";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MyComponent from "./Example/MyComponent";
 
 function App() {
 	return (
-		<>
-			<BrowserRouter>
-				<div className='App'>
-					<header className='App-header'>
-						<Nav />
-						<img src={logo} className='App-logo' alt='logo' />
-
-						<Switch>
-							<Route path='/' exact>
-								<Home />
-							</Route>
-							<Route path='/todos'>
-								<ToDoList />
-							</Route>
-							<Route path='/about'>
-								<MyComponent />
-							</Route>
-						</Switch>
-					</header>
-				</div>
+		<BrowserRouter>
+			<div className='App'>
+				<header className='App-header'>
+					<Nav />
+					<img src={logo} className='App-logo' alt='logo' />
+					<Switch>
+						<Route path='/' exact component={Home}></Route>
+						<Route path='/todos'>
+							<ToDoList />
+						</Route>
+						<Route path='/about'>
+							<MyComponent />
+						</Route>
+					</Switch>
+				</header>
 				<ToastContainer
 					position='top-right'
 					autoClose={5000}
@@ -44,8 +38,8 @@ function App() {
 					theme='light'
 					transition={Bounce}
 				/>
-			</BrowserRouter>
-		</>
+			</div>
+		</BrowserRouter>
 	);
 }
 
