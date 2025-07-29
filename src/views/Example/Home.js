49fb1f5945c2c0845a04dbdc6ea96d81f33836a9
Hue Nhu <hnhu07012004@gmail.com>
2025-07-28 22:59:59 -0700

    Lesson #22: React Routers

---
+++ src/views/Example/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Home extends React.Component {
+	state = {};
+	render() {
+		return <div>Hello World From Home Page With Tako</div>;
+	}
+}
+
+export default Home;
---
+++ src/views/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Nav.scss";
+
+class Nav extends React.Component {
+	state = {};
+	render() {
+		return (
+			<div class='topnav'>
+				<a class='active' href='/'>
+					Home
+				</a>
+				<a href='/todos'>Todos</a>
+				<a href='/about'>About</a>
+			</div>
+		);
+	}
+}
+
+export default Nav;
