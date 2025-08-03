20e8a4437bba1a9a62d4eae4ea65a36ba537ecd6
Hue Nhu <hnhu07012004@gmail.com>
2025-08-03 00:18:43 -0700

    Lesson #32: Setup Redux

---
+++ src/store/reducers/rootReducers.js
@@ -0,0 +1,12 @@
+const initState = {
+	users: [
+		{ id: "01", name: "Hue Nhu" },
+		{ id: "02", name: "Trong Thang" },
+	],
+};
+
+const rootReducer = (state = initState, action) => {
+	return state;
+};
+
+export default rootReducer;
